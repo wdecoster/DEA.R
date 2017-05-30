9e781c010b86843c5e6d9bf7c21fe6c8aaeb1879
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-05-30 17:08:57 +0200

    refactored code

--- SimpleViolin.R
+++ SimpleViolin.R
@@ -18,28 +18,39 @@ processTargets <- function(targets, counts) {
 		}
 	}
 
-args = unlist(strsplit(commandArgs(trailingOnly = TRUE)," "))
-if (length(args) != 3) {
-	giveError("USAGE: SimpleViolin.R countsfile.txt sample_info_file.txt targets.\nTargets are either\n-A comma separated list of gene symbols\n-A differential expression result.")
-} else {
-	counts <- read.table(args[1], stringsAsFactors=F, header=T, row.names=1)
-	sampleInfo <- read.table(args[2], stringsAsFactors=F, header=T)
-	if (!"condition" %in% names(sampleInfo)) {giveError('ERROR: Could not find required field <condition> in sample info file.')} #A required field in sample file is "condition"
-	targets <- processTargets(args[3], counts)
-	for (gene in targets) {
-		dfcounts <- data.frame(condition = sampleInfo$condition, gene = t(counts[gene, ! names(counts) == "symbol"]))
-		p <- ggplot(dfcounts, aes_string(factor("condition"), gene, fill="condition")) +
-			geom_violin() +
-			geom_dotplot(binaxis='y', stackdir='center', dotsize=0.3, binwidth=0.05, position="dodge") +
-			scale_fill_brewer(palette="Dark2") +
-			ggtitle(gene) +
-			theme(axis.title.x = element_blank(),
-				axis.ticks.x=element_blank(),
-				axis.text.x=element_blank(),
-				panel.grid.major.x = element_blank(),
-				plot.title = element_text(hjust = 0.5)) +
-			ylab("rlog-normalized count")
-		labelorder <- levels(factor(dfcounts$subcondition))
-		suppressMessages(ggsave(paste("ViolinPlot_", gene, ".jpeg", sep=""), p))
+makeViolin <- function(gene, conditions, genecounts) {
+	p <- ggplot(data.frame(condition=conditions, gene=genecounts), aes_string(factor("condition"), gene, fill="condition")) +
+		geom_violin() +
+		geom_dotplot(binaxis='y', stackdir='center', dotsize=0.3, binwidth=0.05, position="dodge") +
+		scale_fill_brewer(palette="Dark2") +
+		ggtitle(gene) +
+		theme(axis.title.x = element_blank(),
+			axis.ticks.x=element_blank(),
+			axis.text.x=element_blank(),
+			panel.grid.major.x = element_blank(),
+			plot.title = element_text(hjust = 0.5)) +
+		ylab("rlog-normalized count")
+	suppressMessages(ggsave(paste("ViolinPlot_", gene, ".jpeg", sep=""), p))
+	}
+
+sanityCheck <- function(){
+	args = unlist(strsplit(commandArgs(trailingOnly = TRUE)," "))
+	data = list()
+	if (length(args) != 3) {
+		giveError("USAGE: SimpleViolin.R countsfile.txt sample_info_file.txt targets.\nTargets are either\n-A comma separated list of gene symbols\n-A differential expression result.")
+	} else {
+		data$counts <- read.table(args[1], stringsAsFactors=F, header=T, row.names=1)
+		data$sampleInfo <- read.table(args[2], stringsAsFactors=F, header=T)
+		if (!"condition" %in% names(data$sampleInfo)) {giveError('ERROR: Could not find required field <condition> in sample info file.')} #A required field in sample file is "condition"
+		data$targets <- processTargets(args[3], data$counts)
+		return(data)
 	}
 }
+
+data = sanityCheck()
+for (gene in data$targets) {
+	makeViolin(
+		gene=gene,
+		conditions=data$sampleInfo$condition,
+		genecounts=t(data$counts[gene, ! names(data$counts) == "symbol"]))
+	}
