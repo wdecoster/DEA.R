824e159d1e83d00cda9819f7c19a89637be65d1a
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-05-30 17:57:21 +0200

    read databases from file

--- enrichr-cli.py
+++ enrichr-cli.py
@@ -8,23 +8,18 @@ import sys
 import os
 import argparse
 
-
-databases = ['Achilles_fitness_decrease', 'Achilles_fitness_increase', 'Aging_Perturbations_from_GEO_down', 'Aging_Perturbations_from_GEO_up',
-	'Allen_Brain_Atlas_down', 'Allen_Brain_Atlas_up', 'BioCarta_2013', 'BioCarta_2015', 'BioCarta_2016', 'Cancer_Cell_Line_Encyclopedia', 'ChEA_2013', 'ChEA_2015',
-	'Chromosome_Location', 'CORUM', 'dbGaP', 'Disease_Perturbations_from_GEO_down', 'Disease_Perturbations_from_GEO_up', 'Disease_Signatures_from_GEO_down_2014', 'Disease_Signatures_from_GEO_up_2014',
-	'Drug_Perturbations_from_GEO_2014', 'Drug_Perturbations_from_GEO_down', 'Drug_Perturbations_from_GEO_up', 'ENCODE_and_ChEA_Consensus_TFs_from_ChIP-X',
-	'ENCODE_Histone_Modifications_2013', 'ENCODE_Histone_Modifications_2015', 'ENCODE_TF_ChIP-seq_2014', 'ENCODE_TF_ChIP-seq_2015', 'Epigenomics_Roadmap_HM_ChIP-seq',
-	'ESCAPE', 'Genes_Associated_with_NIH_Grants', 'GeneSigDB', 'Genome_Browser_PWMs', 'GO_Biological_Process_2013', 'GO_Biological_Process_2015', 'GO_Cellular_Component_2013',
-	'GO_Cellular_Component_2015', 'GO_Molecular_Function_2013', 'GO_Molecular_Function_2015', 'GTEx_Tissue_Sample_Gene_Expression_Profiles_down', 'GTEx_Tissue_Sample_Gene_Expression_Profiles_up',
-	'HMDB_Metabolites', 'HomoloGene', 'Human_Gene_Atlas', 'Human_Phenotype_Ontology', 'HumanCyc_2015', 'Humancyc_2016', 'KEA_2013', 'KEA_2015', 'KEGG_2013', 'KEGG_2015', 'KEGG_2016',
-	'Kinase_Perturbations_from_GEO_down', 'Kinase_Perturbations_from_GEO_up', 'Ligand_Perturbations_from_GEO_down', 'Ligand_Perturbations_from_GEO_up', 'LINCS_L1000_Chem_Pert_down',
-	'LINCS_L1000_Chem_Pert_up', 'LINCS_L1000_Kinase_Perturbations_down', 'LINCS_L1000_Kinase_Perturbations_up', 'LINCS_L1000_Ligand_Perturbations_down', 'LINCS_L1000_Ligand_Perturbations_up',
-	'MCF7_Perturbations_from_GEO_down', 'MCF7_Perturbations_from_GEO_up', 'MGI_Mammalian_Phenotype_2013', 'MGI_Mammalian_Phenotype_Level_3', 'MGI_Mammalian_Phenotype_Level_4',
-	'Microbe_Perturbations_from_GEO_down', 'Microbe_Perturbations_from_GEO_up', 'Mouse_Gene_Atlas', 'MSigDB_Computational', 'MSigDB_Oncogenic_Signatures', 'NCI-60_Cancer_Cell_Lines',
-	'NCI-Nature_2015', 'NCI-Nature_2016', 'NURSA_Human_Endogenous_Complexome', 'Old_CMAP_down', 'Old_CMAP_up', 'OMIM_Disease', 'OMIM_Expanded', 'Panther_2015', 'Panther_2016', 'Pfam_InterPro_Domains',
-	'Phosphatase_Substrates_from_DEPOD', 'PPI_Hub_Proteins', 'Reactome_2013', 'Reactome_2015', 'Reactome_2016', 'SILAC_Phosphoproteomics', 'Single_Gene_Perturbations_from_GEO_down',
-	'Single_Gene_Perturbations_from_GEO_up', 'TargetScan_microRNA', 'TF-LOF_Expression_from_GEO', 'Tissue_Protein_Expression_from_Human_Proteome_Map', 'Tissue_Protein_Expression_from_ProteomicsDB',
-	'Transcription_Factor_PPIs', 'TRANSFAC_and_JASPAR_PWMs', 'Virus_Perturbations_from_GEO_down', 'Virus_Perturbations_from_GEO_up', 'VirusMINT', 'WikiPathways_2013', 'WikiPathways_2015', 'WikiPathways_2016']
+def readDBfile():
+	'''
+	Reads possible databases from a file in the installation dir of this script.
+	Dangerous if stuff is moved around!
+	Symlinks are okay.
+	'''
+	db = os.path.join(os.path.dirname(os.path.realpath(__file__)), "enrichr-databases.txt")
+	if not os.path.isfile(db):
+		sys.exit("ERROR: Could not find my database location. Did you move files around?")
+	else:
+		with open(db) as databases:
+			return([line.strip() for line in databases.readlines() if not line == ""])
 
 
 def getArgs():
@@ -135,6 +130,7 @@ def getresults(id, gene_set_library):
 
 if __name__ == '__main__':
 	args = getArgs()
+	databases = readDBfile()
 	genelist = procesinput()
 	id = senddata(genelist)
 	for db in whichdb():
